684a3718b5c35b7716a363d61e343a154d5a27f8
Ella Johnsen <47972861+ellafj@users.noreply.github.com>
2019-10-17 06:59:52 +0200

    upload

---
+++ Project2.py
@@ -0,0 +1,81 @@
+import scipy as sc
+import numpy as np
+from matplotlib import pyplot as plt
+
+A = np.zeros((5,5))
+for i in range(5):
+    if i == 0:
+        A[0,-1] = 1
+    else:
+        A[i, i-1] = 1
+
+print(A)
+
+x0 = np.zeros(5)
+
+b = np.zeros(5)
+b[0] = 1
+
+def GMRES(A, b, x0):
+    m = len(A)
+    j = 0
+
+    r0 = b - A.dot(x0)
+    beta = np.linalg.norm(r0,2)
+    end_not_reached = True
+
+    v = np.zeros((m+1,m))
+    v[0] = r0/beta
+    w = []
+    H = np.zeros((m+1,m))
+
+    while end_not_reached:
+        w.append(A.dot(v[j]))
+
+        for i in range(j+1):
+            H[i,j] = np.inner(w[j], v[i])
+            w[j] = w[j] - H[i,j]*v[i]
+
+        h = np.linalg.norm(w[j], 2)
+
+        if int(h) == 0:
+            print('Found H')
+            print(m)
+            print(H)
+            plt.spy(H)
+            #plt.show()
+            #break
+            return H
+
+        if j != j-1:
+            H[j+1,j] = h
+            v[j+1] = w[j]/H[j+1,j]
+
+        print('H',H)
+        j += 1
+
+H = GMRES(A,b,x0)
+
+def givens_rotations(H, m):
+    #identity = np.eye(m+1)
+    submatrix = np.zeros((2,2))
+    s1 = H[1,0]/np.sqrt(H[0,0]**2+H[1,0]**2)
+    c1 = H[0,0]/np.sqrt(H[0,0]**2+H[1,0]**2)
+    submatrix[0,1] = s1
+    submatrix[1,0] = -s1
+    submatrix[0,0] = c1
+    submatrix[1,1] = c1
+    H_list = [H]
+
+    for i in range(m):
+        omega = np.eye(m+1)
+        omega[i:i+2,i:i+2] = submatrix
+        print(H[i])
+        H_list.append(omega.dot(H_list[i]))
+    print('H_list', H_list)
+
+
+
+
+givens_rotations(H,5)
+
