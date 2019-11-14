567a6b66e71be4ae8d8df7647f6d934a473bcc88
Ella Johnsen <47972861+ellafj@users.noreply.github.com>
2019-11-14 15:41:56 +0100

    PageRank done

--- Assignment 3/PageRank.py
+++ Assignment 3/PageRank.py
@@ -1,45 +1,46 @@
 import numpy as np
 import scipy.io
 import scipy.sparse as sparse
-
-#A = scipy.sparse.coo_matrix([[0,1,0,0,1,0], [1,0,1,0,0,0], [0,0,0,0,0,1], [1,0,0,0,1,0], [1,0,0,1,0,0], [1,1,1,1,1,1]])
+import matplotlib.pyplot as plt
 
 def power_iteration(d, tol):
     # Initializing variables
     A = scipy.io.mmread('stanford.mtx')
     N = A.get_shape()
+    residuals = [1]
+    r = 1
+    iter = 0
     x0 = np.array(np.ones((N[0],1))) / (N[0])
-    print('x0', x0.shape)
 
     # Calculating B-matrix
     k = 1/sparse.csc_matrix.getnnz(A.tocsc(),axis=0)
     B = A.multiply(k)
 
-    #print('hello')
-    #x = np.array(np.ones((N[0],1)))
-
-    r = 1
-
     while r > tol:
-        # Mx = M1 + M2
-        M1 = (1-d)*sum(x0)*np.ones((N[0],1))
-        #print(M1.get_shape())
-        M2 = 0.25 * B.dot(x0)
-        #print(M2.get_shape())
+        iter += 1
+
+        M1 = (1-d)*sum(x0)*np.ones((N[0],1))/N[0]
+        M2 = d * B.dot(x0)
         Mx = M1 + M2
-        #print(Mx.get_shape())
+
         x = Mx / np.linalg.norm(Mx)
-        print('x', x.shape)
         r = np.linalg.norm(x - x0)
-        #print(r)
+        residuals.append(r)
         x0 = x
 
-    return x0
+    return x0, residuals, iter
 
-#print(np.ones((5,1)))
+# Please give up to a minute for this to run
+d = 0.85
+tol = 1E-8
+x, residuals, iter = power_iteration(d, tol)
 
-x = power_iteration(0.85, 1E-8)
-print(x.shape)
-print(np.max(x))
-indexes = np.where(x == np.max(x))
-print(indexes[0])
+print('Used %d iterations to obtain a tolerance of %s' % (iter, tol), '\n')
+print('The most visited site has index', (np.where(x == np.max(x)))[0])
+
+iterations = np.linspace(0, iter, len(residuals))
+plt.semilogy(iterations, residuals, 'k.')
+plt.xlabel('Iterations')
+plt.ylabel('Residuals')
+plt.title('Convergence of Residuals')
+plt.show()
